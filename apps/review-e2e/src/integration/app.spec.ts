bab4dac0007525cd32b9a68cdd9ca0e422505347
Danny <mtme13@hotmail.com>
2020-05-19 19:29:27 +0100

    Test and style main page * Add emotion styled component for list of games * add image assets * Add specs for main page

--- apps/review-e2e/src/integration/app.spec.ts
+++ apps/review-e2e/src/integration/app.spec.ts
@@ -1,7 +1,22 @@
 describe('review', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display list of games', () => {
     cy.contains('Board Game Hoard: Review');
+
+    cy.contains('a', 'Settlers in the Can').within(() => {
+      cy.contains('/5');
+      cy.contains('£35.00');
+    });
+
+    cy.contains('a', 'Chess Pie').within(() => {
+      cy.contains('/5');
+      cy.contains('£15.00');
+    });
+
+    cy.contains('a', 'Purrfection').within(() => {
+      cy.contains('/5');
+      cy.contains('£45.00');
+    });
   });
 });
