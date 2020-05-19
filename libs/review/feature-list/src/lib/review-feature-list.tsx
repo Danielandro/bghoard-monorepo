299d2df958030f4cea9199fbc7b83d82929fa29c
Danny <mtme13@hotmail.com>
2020-05-19 23:31:35 +0100

    Use libs to split code - example of code org * tile component for each game * review list & details components * data access - get games from api * utils - formatter functions * install react-router-dom

---
+++ libs/review/feature-list/src/lib/review-feature-list.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import {
+  Tile,
+  TileGrid,
+  TileLeftCorner,
+  TileRightCorner,
+  TileTitle,
+  Image,
+  ImageWrapper
+} from "@bghoard/review/ui-tile";
+import { currencyFormat, ratingFormat } from "@bghoard/review/util-formatters";
+import { useGames } from "@bghoard/review/data-access-games";
+
+
+/* eslint-disable-next-line */
+export interface ReviewFeatureListProps { }
+
+
+export const ReviewFeatureList = (props: ReviewFeatureListProps) => {
+  const games = useGames();
+  return (
+    <TileGrid>
+      {games.map(game => {
+        return (
+          <a
+            href={'/' + game.id}
+            key={game.id}
+            style={{ textDecoration: 'none', color: 'inherit' }}
+          >
+            <Tile>
+              {game.image && (
+                <ImageWrapper>
+                  <Image src={game.image} />
+                </ImageWrapper>
+              )}
+              <TileTitle>{game.name}</TileTitle>
+              <TileLeftCorner>
+                {ratingFormat(game.rating)}
+              </TileLeftCorner>
+              <TileRightCorner>
+                {currencyFormat(game.price)}
+              </TileRightCorner>
+            </Tile>
+          </a>
+        );
+      })}
+    </TileGrid>
+  );
+};
+
+export default ReviewFeatureList;
